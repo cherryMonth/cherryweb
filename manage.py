6dabdf64c861021a7e1c7fa690cd05d7fb03dbd1
CherryMonth <1115064450@qq.com>
2017-10-19 16:21:04 +0800

    b

--- manage.py
+++ manage.py
@@ -4,22 +4,39 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.options import define, options
 from tornado.ioloop import IOLoop
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)  # 用bootstrap初始化app
+moment = Moment(app)  # 用Moment初始化app 获取本地时间
+
+
+@app.errorhandler(404)
+def page_not_found():
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    user_agent = request.headers.get("User-Agent")
-    return render_template("index.html", user_agent=user_agent)
+    return render_template("index.html", current_time=datetime.datetime.utcnow())
 
 
-# define("port", default=6235, type=int)
+define("port", default=1234, type=int)
 
 if __name__ == '__main__':
-    # http_server = HTTPServer(WSGIContainer(app))
-    # http_server.listen(options.port)  # flask默认的端口
-    # print 'http://127.0.0.1:%d' % options.port
-    # IOLoop.instance().start()
-    app.run(debug=True, host="127.0.0.1", port=1234)
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(options.port)  # flask默认的端口
+    print 'http://127.0.0.1:%d' % options.port
+    IOLoop.instance().start()
